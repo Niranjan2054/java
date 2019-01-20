af098d6108af15e7ecc3ec48b35a9bb5847c1d07
Niranjan Bekoju <bekoju.niranjan@gmail.com>
2019-01-20 14:24:52 +0545

    Decode Class Roll no

--- decodeCRN/src/decodecrn/DecodeCRN.java
+++ decodeCRN/src/decodecrn/DecodeCRN.java
@@ -11,6 +11,15 @@ public class DecodeCRN {
 
     public static void main(String[] args) {
         String crn = JOptionPane.showInputDialog("Enter YOur ROll Number");
-        JOptionPane.showMessageDialog(null, "Batch: " + crn.substring(3, 6) + "\nDept: " + crn.substring(6, 9) + "\nRoll: " + crn.substring(9, 12), "YOur College Information", 1);
+        try {
+            int length = crn.length();
+            if (length == 12) {
+                JOptionPane.showMessageDialog(null, "Batch: " + crn.substring(3, 6) + "\nDept: " + crn.substring(6, 9) + "\nRoll: " + crn.substring(9, 12), "YOur College Information", 1);
+            } else {
+                throw new Exception("Roll no should of format 'KCE074BCT025'");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error Message:\n" + e.getMessage(), "ERROR", 0);
+        }
     }
 }
